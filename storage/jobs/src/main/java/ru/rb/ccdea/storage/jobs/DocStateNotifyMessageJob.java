fe897e1ae52da9f4ff0a05d9c32d77d5a5107ae5
SotnikovAV <SotnikovAV@rbtechnologies.ru>
2016-03-15 15:17:55 +0300

    Рефакторинг для удобства тестирования

--- storage/jobs/src/main/java/ru/rb/ccdea/storage/jobs/DocStateNotifyMessageJob.java
+++ storage/jobs/src/main/java/ru/rb/ccdea/storage/jobs/DocStateNotifyMessageJob.java
@@ -9,6 +9,7 @@ import com.documentum.fc.client.IDfSession;
 import com.documentum.fc.client.IDfSessionManager;
 import com.documentum.fc.client.IDfSysObject;
 import com.documentum.fc.common.DfException;
+import com.documentum.fc.common.DfId;
 import com.documentum.fc.common.DfLogger;
 import com.documentum.fc.common.IDfId;
 import com.documentum.fc.common.IDfLoginInfo;
@@ -30,67 +31,71 @@ public class DocStateNotifyMessageJob extends AbstractJob{
     private void process(IDfSession dfSession) throws DfException {
     	List<IDfId> messageIdList = ExternalMessagePersistence.getLoadedMessageList(dfSession);
         for (IDfId messageId : messageIdList) {
-            boolean isTransAlreadyActive = dfSession.isTransactionActive();
-            try {
+        	process(dfSession, messageId);
+        }
+    }
+    
+    public void process(IDfSession dfSession, IDfId messageId) throws DfException {
+    	boolean isTransAlreadyActive = dfSession.isTransactionActive();
+        try {
 
-                DfLogger.debug(this, "Start MessageID: {0}", new String[]{messageId.getId()}, null);
+            DfLogger.debug(this, "Start MessageID: {0}", new String[]{messageId.getId()}, null);
 
-                if (!isTransAlreadyActive) {
-                    dfSession.beginTrans();
-                }
+            if (!isTransAlreadyActive) {
+                dfSession.beginTrans();
+            }
 
-                IDfSysObject messageObject = (IDfSysObject)dfSession.getObject(messageId);
-                String ctsJobResult = ExternalMessagePersistence.getContentProcessingByCTSIfFinished(messageObject);
-				if (ctsJobResult == null) {
-					IDfSysObject originalContentObject = ExternalMessagePersistence
-							.getOriginalMessageContentObject(messageObject);
-					if (originalContentObject.getContentSize() > 0
-							&& FileFormat.PDF.value().equalsIgnoreCase(originalContentObject.getContentType())) {
-						ctsJobResult = CTSRequestBuilder.RESPONSE_STATUS_COMPLITED;
-					}
+            IDfSysObject messageObject = (IDfSysObject)dfSession.getObject(messageId);
+            String ctsJobResult = ExternalMessagePersistence.getContentProcessingByCTSIfFinished(messageObject);
+			if (ctsJobResult == null) {
+				IDfSysObject originalContentObject = ExternalMessagePersistence
+						.getOriginalMessageContentObject(messageObject);
+				if (originalContentObject.getContentSize() > 0
+						&& FileFormat.PDF.value().equalsIgnoreCase(originalContentObject.getContentType())) {
+					ctsJobResult = CTSRequestBuilder.RESPONSE_STATUS_COMPLITED;
 				}
-                if (CTSRequestBuilder.RESPONSE_STATUS_COMPLITED.equalsIgnoreCase(ctsJobResult)) {
-                    IDfSysObject originalContentObject = ExternalMessagePersistence.getOriginalMessageContentObject(messageObject);
-                    IDfSysObject nextContent = ContentPersistence.getNextContentSysObject(originalContentObject, true);
-                    if (nextContent != null) {
-                        DfLogger.info(this, "Process MessageID: {0} - destroy completed content part {1}", new String[] {messageId.getId(), nextContent.getObjectId().getId()}, null);
-                        nextContent.destroy();
-                    }
-                    nextContent = ContentPersistence.getNextContentSysObject(originalContentObject, true);
-                    if (nextContent != null) {
-                        IDfSysObject documentContentObject = ExternalMessagePersistence.getDocumentContentObject(messageObject);
-                        String transformJobId = CTSRequestBuilder.mergePdfRequest(dfSession, documentContentObject.getObjectId().getId(), false, documentContentObject, nextContent);
-                        ExternalMessagePersistence.continueNextContentTransform(messageObject, transformJobId);
-                    }
-                    else {
-                        ExternalMessagePersistence.notifyExternalSystemAboutContentProcessing(messageObject);
-                    }
+			}
+            if (CTSRequestBuilder.RESPONSE_STATUS_COMPLITED.equalsIgnoreCase(ctsJobResult)) {
+                IDfSysObject originalContentObject = ExternalMessagePersistence.getOriginalMessageContentObject(messageObject);
+                IDfSysObject nextContent = ContentPersistence.getNextContentSysObject(originalContentObject, true);
+                if (nextContent != null) {
+                    DfLogger.info(this, "Process MessageID: {0} - destroy completed content part {1}", new String[] {messageId.getId(), nextContent.getObjectId().getId()}, null);
+                    nextContent.destroy();
                 }
-                else if (CTSRequestBuilder.RESPONSE_STATUS_FAILED.equalsIgnoreCase(ctsJobResult)) {
-                    ExternalMessagePersistence.setConvertationError(messageObject);
+                nextContent = ContentPersistence.getNextContentSysObject(originalContentObject, true);
+                if (nextContent != null) {
+                    IDfSysObject documentContentObject = ExternalMessagePersistence.getDocumentContentObject(messageObject);
+                    String transformJobId = CTSRequestBuilder.mergePdfRequest(dfSession, documentContentObject.getObjectId().getId(), false, documentContentObject, nextContent);
+                    ExternalMessagePersistence.continueNextContentTransform(messageObject, transformJobId);
                 }
                 else {
-                    DfLogger.debug(this, "Skip MessageID: {0} because content not ready or empty", new String[] {messageId.getId()}, null);
+                    ExternalMessagePersistence.notifyExternalSystemAboutContentProcessing(messageObject);
                 }
+            }
+            else if (CTSRequestBuilder.RESPONSE_STATUS_FAILED.equalsIgnoreCase(ctsJobResult)) {
+                ExternalMessagePersistence.setConvertationError(messageObject);
+            }
+            else {
+                DfLogger.debug(this, "Skip MessageID: {0} because content not ready or empty", new String[] {messageId.getId()}, null);
+            }
 
-                if (!isTransAlreadyActive) {
-                    dfSession.commitTrans();
-                }
+            if (!isTransAlreadyActive) {
+                dfSession.commitTrans();
+            }
 
-                DfLogger.debug(this, "Finish MessageID: {0}", new String[] {messageId.getId()}, null);
-            }
-            catch (DfException dfEx) {
-                DfLogger.error(this, "Error MessageID: {0}", new String[] {messageId.getId()}, dfEx);
-                //throw dfEx;
-            }
-            catch (Exception ex) {
-                DfLogger.error(this, "Error MessageID: {0}", new String[] {messageId.getId()}, ex);
-                //throw new DfException(ex);
-            }
-            finally {
-                if (!isTransAlreadyActive && dfSession.isTransactionActive()) {
-                    dfSession.abortTrans();
-                }
+            DfLogger.debug(this, "Finish MessageID: {0}", new String[] {messageId.getId()}, null);
+        }
+        catch (DfException dfEx) {
+            DfLogger.error(this, "Error MessageID: {0}", new String[] {messageId.getId()}, dfEx);
+            //throw dfEx;
+        }
+        catch (Exception ex) {
+            DfLogger.error(this, "Error MessageID: {0}", new String[] {messageId.getId()}, ex);
+            //throw new DfException(ex);
+        }
+        finally {
+            if (!isTransAlreadyActive && dfSession.isTransactionActive()) {
+                dfSession.abortTrans();
             }
         }
     }
@@ -146,8 +151,25 @@ public class DocStateNotifyMessageJob extends AbstractJob{
             testSession = sessionManager.getSession("UCB");
             
             DocStateNotifyMessageJob job = new DocStateNotifyMessageJob();
-            job.process(testSession);
-            job.updateWaitingContents(testSession);
+
+			String messageIdStr = null;
+			if (args != null && args.length > 0) {
+				messageIdStr = args[0];
+			}
+
+			IDfId messageId = null;
+			if (messageIdStr != null) {
+				messageId = new DfId(messageIdStr);
+			}
+			
+			if (messageId != null && !messageId.isNull() && messageId.isObjectId()) {
+				job.process(testSession, messageId);
+			} else {
+				job.process(testSession);
+	            job.updateWaitingContents(testSession);
+			}
+            
+            
             
         } catch (Exception ex) {
             ex.printStackTrace();

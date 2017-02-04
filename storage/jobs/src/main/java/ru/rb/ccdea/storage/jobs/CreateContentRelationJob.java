83025a095b442522cfeb9e4fe673319662d02f88
SotnikovAV <SotnikovAV@rbtechnologies.ru>
2017-02-04 15:39:12 +0300

    Исправлена привязка контента: добавлено разбиение на отдельные транзакции, добавлена синхронизация для обработки в разных потоках (job-ах), добавлена проверка на идентичность контента при обработке версионности

--- storage/jobs/src/main/java/ru/rb/ccdea/storage/jobs/CreateContentRelationJob.java
+++ storage/jobs/src/main/java/ru/rb/ccdea/storage/jobs/CreateContentRelationJob.java
@@ -16,130 +16,151 @@ import ru.rb.ccdea.storage.persistence.fileutils.ContentLoader;
 
 public class CreateContentRelationJob extends AbstractJob {
 
-    public static void main(String[] args) {
-        IDfSession testSession = null;
-        IDfClientX clientx = new DfClientX();
-        IDfClient client = null;
-        IDfSessionManager sessionManager = null;
-        try {
-            client = clientx.getLocalClient();
-            sessionManager = client.newSessionManager();
+	public static void main(String[] args) {
+		IDfSession testSession = null;
+		IDfClientX clientx = new DfClientX();
+		IDfClient client = null;
+		IDfSessionManager sessionManager = null;
+		try {
+			client = clientx.getLocalClient();
+			sessionManager = client.newSessionManager();
 
-            IDfLoginInfo loginInfo = clientx.getLoginInfo();
-            loginInfo.setUser("dmadmin");
-            loginInfo.setPassword("Fkut,hf15");
-            loginInfo.setDomain(null);
+			IDfLoginInfo loginInfo = clientx.getLoginInfo();
+			loginInfo.setUser("dmadmin");
+			loginInfo.setPassword("dmadmin");
+			loginInfo.setDomain(null);
 
-            sessionManager.setIdentity("ELAR", loginInfo);
-            testSession = sessionManager.getSession("ELAR");
+			sessionManager.setIdentity("UCB", loginInfo);
+			testSession = sessionManager.getSession("UCB");
 
-            CreateContentRelationJob job = new CreateContentRelationJob();
-            job.process(testSession);
+			CreateContentRelationJob job = new CreateContentRelationJob();
+			job.process(testSession);
 
-        } catch (Exception ex) {
-            System.out.println(ex);
-        } finally {
-            if (testSession != null) {
-                sessionManager.release(testSession);
-            }
-        }
-    }
+		} catch (Exception ex) {
+			System.out.println(ex);
+		} finally {
+			if (testSession != null) {
+				sessionManager.release(testSession);
+			}
+		}
+	}
 
-    @Override
-    public int execute() throws Exception {
-        process(dfSession);
-        return 0;
-    }
+	@Override
+	public int execute() throws Exception {
+		process(dfSession);
+		return 0;
+	}
 
-    public void process(IDfSession dfSession) throws DfException {
+	public void process(IDfSession dfSession) throws DfException {
 
 		boolean isTransAlreadyActive = dfSession.isTransactionActive();
-		String dql = "select distinct d.r_object_id as d_id, d.r_object_type as d_type, c.r_object_id as c_id, c.r_creation_date from ccdea_doc_content c, ccdea_base_doc d where  upper(c.rp_doc_source_code)=upper(d.rp_content_source_code) and c.rp_doc_source_id=d.rp_content_source_id and not exists(select r_object_id from dm_relation where relation_name='ccdea_content_relation'and parent_id=d.r_object_id and child_id=c.i_chronicle_id) order by c.r_creation_date enable (ROW_BASED)";
-		
-        try {
-            DfLogger.info(this, "Start CreateContentRelationJob ", null, null);
+		String dql = "select "
+				+ "distinct d.r_object_id as d_id, d.r_object_type as d_type, c.r_object_id as c_id, c.r_creation_date "
+				+ "from ccdea_doc_content c, ccdea_base_doc d "
+				+ "where  upper(c.rp_doc_source_code)=upper(d.rp_content_source_code) "
+				+ "and c.rp_doc_source_id=d.rp_content_source_id "
+				+ "and not exists("
+				+ "select r_object_id from dm_relation "
+				+ "where relation_name='ccdea_content_relation'"
+				+ "and parent_id=d.r_object_id and child_id=c.i_chronicle_id"
+				+ ") order by c.r_creation_date enable (ROW_BASED)";
 
-            long start = System.currentTimeMillis();
+		try {
+			DfLogger.info(this, "Start CreateContentRelationJob ", null, null);
 
-            // старт транзакции
-            if (!isTransAlreadyActive) {
-                dfSession.beginTrans();
-            }
+			long start = System.currentTimeMillis();
 
-            // обработка результатов запроса
-            IDfQuery query = new DfQuery();
-            IDfCollection collection = null;
+			// обработка результатов запроса
+			IDfQuery query = new DfQuery();
+			IDfCollection collection = null;
 
-            try {
-                query.setDQL(dql);
-                collection = query.execute(dfSession, IDfQuery.READ_QUERY);
-                Set<String> processedContentIds = new HashSet<String>();
+			try {
+				query.setDQL(dql);
+				collection = query.execute(dfSession, IDfQuery.READ_QUERY);
+				Set<String> processedContentIds = new HashSet<String>();
 				while (collection != null && collection.next()) {
-					IDfId baseDocId = collection.getId("d_id");
-					IDfId docContentId = collection.getId("c_id");
-					String baseDocType = collection.getString("d_type");
 					try {
-						if (ContentPersistence.isDocTypeSupportContentVersion(baseDocType)) {
-							IDfSysObject contentObj = (IDfSysObject) dfSession.getObjectByQualification(
-									"ccdea_doc_content where i_chronicle_id in (select child_id from dm_relation where relation_name='ccdea_content_relation' and parent_id="
-											+ DfUtil.toQuotedString(baseDocId.getId()) + ")");
-							if (contentObj != null && !processedContentIds.contains(docContentId.getId())) {
-								processedContentIds.add(docContentId.getId());
-								IDfSysObject newContentObj = (IDfSysObject) dfSession.getObject(docContentId);
-								if (newContentObj != null) {
-									try {
-										ContentLoader.saveContent(contentObj, newContentObj.getContentType(),
-												newContentObj.getContent(), true);
-										newContentObj.destroy();
-									} catch (DfException e) {
-										throw new DfException("error while creating content with new id="
-												+ newContentObj.getObjectId().getId() + " old id="
-												+ contentObj.getObjectId().getId(), e);
-									}
-								}
-								continue;
-							}
+						// старт транзакции
+						if (!isTransAlreadyActive) {
+							dfSession.beginTrans();
 						}
-						ContentPersistence.createDocumentContentRelation(dfSession, baseDocId, docContentId);
+
+						IDfId baseDocId = collection.getId("d_id");
+						IDfId docContentId = collection.getId("c_id");
+						String baseDocType = collection.getString("d_type");
+						IDfSysObject doc = (IDfSysObject) dfSession.getObject(baseDocId);
+						try {
+							doc.lock();
+							if (ContentPersistence.isDocTypeSupportContentVersion(baseDocType)) {
+								IDfSysObject contentObj = (IDfSysObject) dfSession.getObjectByQualification(
+										"ccdea_doc_content where i_chronicle_id in (select child_id from dm_relation where relation_name='ccdea_content_relation' and parent_id="
+												+ DfUtil.toQuotedString(baseDocId.getId()) + ")");
+								if (contentObj != null && !processedContentIds.contains(docContentId.getId())
+										&& !contentObj.getObjectId().equals(docContentId)) {
+									processedContentIds.add(docContentId.getId());
+
+									IDfSysObject newContentObj = (IDfSysObject) dfSession.getObject(docContentId);
+									if (newContentObj != null) {
+										try {
+											ContentLoader.saveContent(contentObj, newContentObj.getContentType(),
+													newContentObj.getContent(), true);
+											newContentObj.destroy();
+										} catch (DfException e) {
+											throw new DfException("error while creating content with new id="
+													+ newContentObj.getObjectId().getId() + " old id="
+													+ contentObj.getObjectId().getId(), e);
+										}
+									}
+
+									continue;
+								}
+							}
+							ContentPersistence.createDocumentContentRelation(dfSession, baseDocId, docContentId);
+						} catch (DfException e) {
+							DfLogger.error(this, "Error while creating content with id=" + docContentId
+									+ " for document with id=" + baseDocId, null, e);
+						} finally {
+							doc.save();
+						}
+						
+						// коммит транзакции
+						if (!isTransAlreadyActive && dfSession.isTransactionActive()) {
+							dfSession.commitTrans();
+						}
+						
 					} catch (DfException e) {
-						DfLogger.error(this, "Error while creating content with id=" + docContentId
-								+ " for document with id=" + baseDocId, null, e);
+						DfLogger.error(this, "Error in CreateContentRelationJob JOB {0}",
+								new String[] { e.getMessage() }, e);
+						if (!isTransAlreadyActive && dfSession.isTransactionActive()) {
+							dfSession.abortTrans();
+						}
 					}
 				}
-            } catch (DfException e) {
-                DfLogger.error(this, "Error in CreateContentRelationJob JOB {0}", new String[]{e.getMessage()}, e);
-                if (!isTransAlreadyActive && dfSession.isTransactionActive()) {
-                    dfSession.abortTrans();
-                }
-            } finally {
-                closeCollection(collection);
-            }
+			} finally {
+				closeCollection(collection);
+			}
 
-            // коммит транзакции
-            if (!isTransAlreadyActive && dfSession.isTransactionActive()) {
-                dfSession.commitTrans();
-            }
+			long stop = System.currentTimeMillis();
 
-            long stop = System.currentTimeMillis();
+			DfLogger.info(this, "Finish CreateContentRelationJob in {0} seconds", new Long[] { (stop - start) / 1000 },
+					null);
+		} catch (Exception ex) {
+			DfLogger.error(this, "Error in CreateContentRelationJob JOB: {0}", new String[] { ex.getMessage() }, ex);
+		} finally {
+			if (!isTransAlreadyActive && dfSession.isTransactionActive()) {
+				dfSession.abortTrans();
+			}
+		}
 
-            DfLogger.info(this, "Finish CreateContentRelationJob in {0} seconds", new Long[]{(stop - start) / 1000}, null);
-        } catch (Exception ex) {
-            DfLogger.error(this, "Error in CreateContentRelationJob JOB: {0}", new String[]{ex.getMessage()}, ex);
-        } finally {
-            if (!isTransAlreadyActive && dfSession.isTransactionActive()) {
-                dfSession.abortTrans();
-            }
-        }
+	}
 
-    }
-
-    private void closeCollection(IDfCollection collection) {
-        try {
-            if (collection != null && collection.getState() != IDfCollection.DF_CLOSED_STATE) {
-                collection.close();
-            }
-        } catch (DfException var2) {
-            DfLogger.error(this, "Error in close: {0}", new String[]{var2.getMessage()}, var2);
-        }
-    }
+	private void closeCollection(IDfCollection collection) {
+		try {
+			if (collection != null && collection.getState() != IDfCollection.DF_CLOSED_STATE) {
+				collection.close();
+			}
+		} catch (DfException var2) {
+			DfLogger.error(this, "Error in close: {0}", new String[] { var2.getMessage() }, var2);
+		}
+	}
 }
